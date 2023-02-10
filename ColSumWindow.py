3cb8cffaa28e10c33377dafbdef75ae9da9f6d75
Gabriel Johnson <gjohn018@odu.edu>
2023-02-10 12:23:45 -0500

    Finished GUI

--- ColSumWindow.py
+++ ColSumWindow.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 from ColumnSummationTool import ColumnSummationTool
+import time
 
 class ColSumWindow:
 
     def __init__(self):
+        # If values are set correctly, allowing you to save to file
+        self.canSaveToFile = False
+
         # This section initializes the tkinter window
         self.root = tk.Tk()
+
         self.root.title("Summation Verification Tool")
         # WidthxHeight
-        self.root.geometry("800x120")
+        self.root.geometry("800x150")
         self.wb1Button = tk.Button(self.root, width=20, text="Select Workbook 1", bg="#ffffff",
                               command=lambda: self.selectWB(workbookNumber=1))
         self.wb2Button = tk.Button(self.root, width=20, text="Select Workbook 2", bg="#ffffff",
@@ -17,6 +22,9 @@ class ColSumWindow:
         self.infoLabel = tk.Label(self.root, text="Set both sheets and columns before starting", bg="#FA8A6F")
         self.setButton=tk.Button(self.root, text="Set Values", bg="#32ed35", width=20, command=lambda: self.setVals())
         self.startButton=tk.Button(self.root, text="Start", bg="#FA8A6F", width=20, command=lambda: self.start())
+        self.saveButton=tk.Button(self.root, text="Save current values to file", bg="#FA8A6F", width=20, command=lambda: self.save())
+        self.loadButton=tk.Button(self.root, text="Load values from file", bg="white", width=20, command=lambda: self.load())
+
         self.firstSumLabel = tk.Label(self.root, text="First sum will appear here", bg="#ffffff")
         self.secSumLabel = tk.Label(self.root, text="Second sum will appear here", bg="#ffffff")
 
@@ -47,10 +55,14 @@ class ColSumWindow:
         self.setButton.grid(row=2, column=0, sticky="NSEW")
         self.startButton.grid(row=2, column=1, sticky="NSEW")
 
+        # Fourth Row
+        self.saveButton.grid(row=3, column=0,  sticky="NSEW")
+        self.loadButton.grid(row=3, column=1, sticky="NSEW")
+
         # Info Section
-        self.firstSumLabel.grid(row=3, column=0, columnspan=2, sticky="NSW")
-        self.secSumLabel.grid(row=3, column=2, columnspan=2, sticky="NSW")
-        self.infoLabel.grid(row=4, column=0, columnspan=4, sticky="NSEW")
+        self.firstSumLabel.grid(row=4, column=0, columnspan=2, sticky="NSW")
+        self.secSumLabel.grid(row=4, column=2, columnspan=2, sticky="NSW")
+        self.infoLabel.grid(row=5, column=0, columnspan=4, sticky="NSEW")
 
         self.excelTool = ColumnSummationTool()
 
@@ -61,18 +73,111 @@ class ColSumWindow:
         self.root.mainloop()
 
 
-
     def selectWB(self, workbookNumber):
         fileName = fd.askopenfilename()
         if workbookNumber ==1:
             self.excelTool.openWorkbooks(workbookNumber,fileName)
-
         elif workbookNumber ==2:
             self.excelTool.openWorkbooks(workbookNumber, fileName)
 
-    def start(self):
-        sheet1= str(self.firstSheetEntry.get())
+
+    def setVals(self):
+        sheet1 = str(self.firstSheetEntry.get())
         sheet2 = str(self.secSheetEntry.get())
+        col1 = str(self.firstColEntry.get())
+        col2 = str(self.secColEntry.get())
+
+        if self.excelTool.wb1 == "" or self.excelTool.wb2 =="":
+            self.infoLabel.config(text="Must select excel files first")
+            self.infoLabel.config(bg="red")
+            self.canSaveToFile = False
+            return
+        elif sheet1 == "Enter the first sheet to gather data from" or sheet2 == "Enter the second sheet to gather data from":
+                self.infoLabel.config(text="Must enter sheet names first")
+                self.infoLabel.config(bg="red")
+                self.canSaveToFile = False
+                return
+        elif len(col1) > 5 or len(col2) > 5:
+            self.infoLabel.config(text="Invalid column")
+            self.infoLabel.config(bg="red")
+            self.canSaveToFile = False
+            return
+        else:
+            sheetLoad = self.excelTool.setSheet(sheet1, sheet2)
+            if sheetLoad == False:
+                self.infoLabel.config(text="Error loading sheets by name (see warnings on console)")
+                self.infoLabel.config(bg="red")
+                self.canSaveToFile = False
+                return
+            else:
+                self.infoLabel.config(text="Workbook and sheet values set correctly")
+                self.canSaveToFile=True
+                self.infoLabel.config(bg="#32ed35")
+                self.setButton.config(bg="white")
+                self.startButton.config(bg="#32ed35")
+                self.saveButton.config(bg="#32ed35")
+
+
+
+    def save(self):
+        if self.canSaveToFile == True:
+            wb1 = self.excelTool.wb1Name
+            wb2 = self.excelTool.wb2Name
+            sheet1 = str(self.firstSheetEntry.get())
+            sheet2 = str(self.secSheetEntry.get())
+            col1 = str(self.firstColEntry.get())
+            col2 = str(self.secColEntry.get())
+            saveString = f"{wb1};{wb2};{sheet1};{sheet2};{col1};{col2}"
+            with open("config.txt", "w") as f:
+                f.write(saveString)
+            self.infoLabel.config(text="Config saved")
+            self.infoLabel.config(bg="green")
+        else:
+            self.infoLabel.config(text="Cannot save to config, some values may not be set correctly")
+            self.infoLabel.config(bg="red")
+
+
+    def load(self):
+
+        try:
+            with open("config.txt", "r") as f:
+                line = f.readline()
+            wb1, wb2, sheet1, sheet2, col1, col2 = line.split(";")
+            self.excelTool.openWorkbooks(1, wb1)
+            self.excelTool.openWorkbooks(2, wb2)
+            self.firstSheetEntry.delete(0,tk.END)
+            self.firstSheetEntry.insert(0,sheet1)
+            self.secSheetEntry.delete(0, tk.END)
+            self.secSheetEntry.insert(0, sheet2)
+            self.firstColEntry.delete(0, tk.END)
+            self.firstColEntry.insert(0, col1)
+            self.secColEntry.delete(0, tk.END)
+            self.secColEntry.insert(0, col2)
+            self.infoLabel.config(text="Config Loaded, click set button, then start button")
+            self.infoLabel.config(bg="green")
+        except:
+            self.infoLabel.config(text="Error loading config, set manually instead")
+            self.infoLabel.config(bg="red")
+
+
+
+
+
+
+
+
+
+
+
+    def start(self):
+        col1 = str(self.firstColEntry.get())
+        col2 = str(self.secColEntry.get())
+        sum1, sum2 = self.excelTool.addColumnValues(col1, col2)
+        self.firstSumLabel.config(text=sum1)
+        self.secSumLabel.config(text=sum2)
+        self.infoLabel.config(text="Values Displayed Above")
+        self.infoLabel.config(bg="green")
+
 
 
 if __name__ == "__main__":
--- ColumnSummationTool.py
+++ ColumnSummationTool.py
@@ -8,8 +8,10 @@ class ColumnSummationTool:
     def __init__(self):
         # The first workbook
         self.wb1 = ""
+        self.wb1Name=""
         # The second workbook (may be the same as the first)
         self.wb2 = ""
+        self.wb2Name=""
         # Initialize the first and second worksheets. These will be selected from a dropdown of
         # Available worksheets later
         self.ws1 = ""
@@ -24,11 +26,56 @@ class ColumnSummationTool:
     def openWorkbooks(self, workbookNumber, fileName):
         if workbookNumber == 1:
             self.wb1 = openpyxl.load_workbook(fileName)
+            self.wb1Name = fileName
         elif workbookNumber==2:
             self.wb2 = openpyxl.load_workbook(fileName)
+            self.wb2Name = fileName
 
         print("Loaded workbook {num}".format(num=workbookNumber))
 
 
-    def addCol(self):
-        pass
+    def setSheet(self, sheet1, sheet2):
+        sheet1=str(sheet1)
+        sheet2=str(sheet2)
+        returnCode=True
+        try:
+            self.ws1 = self.wb1[sheet1]
+            print("Loaded {sheet1} from workbook 1".format(sheet1=sheet1))
+        except:
+            print("Could not open sheet {sheet1} in workbook 1".format(sheet1=sheet1))
+            returnCode=False
+        try:
+            self.ws2 = self.wb1[sheet2]
+            print("Loaded {sheet2} from workbook 2".format(sheet2=sheet2))
+        except:
+            print("Could not open sheet {sheet2} in workbook 2".format(sheet2=sheet2))
+            returnCode=False
+        return returnCode
+
+    def addColumnValues(self, col1, col2):
+        sum1 = 0.0
+        sum2 = 0.0
+        returnCode = True
+        position = 0
+        for cell in self.ws1[col1]:
+            position+=1
+            try:
+                value = cell.value
+                value = float(value)
+                sum1+=value
+            except:
+                print("[First Column] Non-number value at row {row} with value {value}".format(row=position, value=value))
+        for cell in self.ws2[col2]:
+            position+=1
+            try:
+                value = float(cell.value)
+                sum2+=value
+            except:
+                print("[First Column] Non-number value at row {row} with value {value}".format(row=position, value=value))
+
+
+
+        return sum1, sum2
+
+
+
